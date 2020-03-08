6e5b06dff167dc93d60536a99674e88e059beea6
Qiumu <sddz_ly@foxmail.com>
2020-03-08 10:34:10 +0800

    init the project

--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './theme/element'
+import Axios from 'axios'
 
 Vue.config.productionTip = false
 
+Vue.prototype.$ajax = Axios
+Axios.defaults.withCredentials = true;
+
 new Vue({
+  el: '#app',
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+})
