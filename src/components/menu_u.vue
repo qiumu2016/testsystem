d8a0c4c3b8d70d554afdd5537fcac86966db163e
Qiumu <sddz_ly@foxmail.com>
2020-03-19 13:44:13 +0800

    完成u设备参数窗口布局

--- src/components/menu_u.vue
+++ src/components/menu_u.vue
@@ -5,7 +5,7 @@
                 <template slot="title">操作</template>
                 <el-menu-item v-for="(value, index) in menu.option" :key="index" :index="1 + '_' + index">{{value}}</el-menu-item>
             </el-submenu>
-            <el-menu-item index="2">控制参数</el-menu-item>
+            <el-menu-item index="2">监控参数</el-menu-item>
             <el-submenu index="3">
                 <template slot="title">监控命令</template>
                 <el-menu-item v-for="(value, index) in menu.monitorCommand" :key="index" :index="2 + '_' + index">{{value}}</el-menu-item>
@@ -24,6 +24,186 @@
                 <el-menu-item v-for="(value, index) in menu.help" :key="index" :index="6 + '_' + index">{{value}}</el-menu-item>
             </el-submenu>
         </el-menu>
+        <el-dialog class="monitorDialog"
+            title="监控命令的参数设置"
+            :visible="monitorDialogVisible"
+            width="45%"
+            center >
+            <el-col :span="12">
+               <el-card class="monitorbox">
+                    <div slot="header" class="clearfix">
+                        <span>设备1参数</span>
+                    </div>
+                    <el-row>
+                        <span>3&nbsp; 设置工作模式 &nbsp;</span>
+                        <el-select v-model="monitorDevice_1.workMode" class="in_put_1" size="small">
+                            <el-option
+                            v-for="item in workMode"
+                            :key="item.value"
+                            :label="item.label"
+                            :value="item.value">
+                            </el-option>
+                        </el-select>
+                    </el-row>
+                    <el-row style="margin-bottom: 10px">
+                        <span>5&nbsp; 设置/查询工组</span>
+                    </el-row>
+                    <el-row>
+                        <span class="text">种类&nbsp;</span>
+                        <el-select v-model="monitorDevice_1.groupType" class="in_put_2" size="small">
+                            <el-option
+                            v-for="item in workType"
+                            :key="item.value"
+                            :label="item.label"
+                            :value="item.value">
+                            </el-option>
+                        </el-select>
+                        <span class="text">组号&nbsp;</span>
+                        <el-select v-model="monitorDevice_1.groupNumber" class="in_put_2" size="small">
+                            <el-option
+                            v-for="item in workNumber"
+                            :key="item.value"
+                            :label="item.label"
+                            :value="item.value">
+                            </el-option>
+                        </el-select>
+                    </el-row>
+                </el-card>
+            </el-col>
+            <el-col :span="12">
+                <el-card class="monitorbox">
+                    <div slot="header" class="clearfix">
+                        <span>设备2参数</span>
+                    </div>
+                    <el-row>
+                        <span>3 &nbsp;设置工作模式 &nbsp;</span>
+                        <el-select v-model="monitorDevice_2.workMode" class="in_put_1" size="small">
+                            <el-option
+                            v-for="item in workMode"
+                            :key="item.value"
+                            :label="item.label"
+                            :value="item.value">
+                            </el-option>
+                        </el-select>
+                    </el-row>
+                    <el-row style="margin-bottom: 10px">
+                        <span>5 &nbsp;设置/查询工组 &nbsp;</span>
+                    </el-row>
+                    <el-row>
+                        <span class="text">种类</span>
+                        <el-select v-model="monitorDevice_2.groupType" class="in_put_2" size="small">
+                            <el-option
+                            v-for="item in workType"
+                            :key="item.value"
+                            :label="item.label"
+                            :value="item.value">
+                            </el-option>
+                        </el-select>
+                        <span class="text">组号</span>
+                        <el-select v-model="monitorDevice_2.groupNumber" class="in_put_2" size="small">
+                            <el-option
+                            v-for="item in workNumber"
+                            :key="item.value"
+                            :label="item.label"
+                            :value="item.value">
+                            </el-option>
+                        </el-select>
+                    </el-row>
+                </el-card>
+            </el-col>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="monitorDialogCancel()">取 消</el-button>
+                <el-button type="primary" @click="monitorDialogCancel()">确 定</el-button>
+            </span>
+        </el-dialog>
+        <el-dialog class="monitorDialog"
+            title="测评命令的参数设置"
+            :visible="testDialogVisible"
+            width="45%"
+            center >
+            <el-col :span="12">
+               <el-card class="monitorbox">
+                    <div slot="header" class="clearfix">
+                        <span>设备1参数</span>
+                    </div>
+                    <el-row style="margin-bottom: 10px">
+                        <span class="text2">IK模式 &nbsp;</span>
+                        <el-select v-model="testDevice_1.IKMode" class="in_put_1" size="small">
+                            <el-option
+                            v-for="item in IKMode"
+                            :key="item.value"
+                            :label="item.label"
+                            :value="item.value">
+                            </el-option>
+                        </el-select>
+                    </el-row>
+                    <el-row style="margin-bottom: 10px">
+                        <span class="text2">IK参数  &nbsp;</span>
+                        <el-input v-model="testDevice_1.IKPara" class="in_put_1" size="small">
+                        </el-input>
+                    </el-row>
+                   <el-row style="margin-bottom: 10px">
+                        <span class="text">WK模式 &nbsp;</span>
+                        <el-select v-model="testDevice_1.WKMode" class="in_put_1" size="small">
+                            <el-option
+                            v-for="item in WKMode"
+                            :key="item.value"
+                            :label="item.label"
+                            :value="item.value">
+                            </el-option>
+                        </el-select>
+                    </el-row>
+                    <el-row style="margin-bottom: 10px">
+                        <span class="text">WK参数  &nbsp;</span>
+                        <el-input type="textarea" :rows=3 v-model="testDevice_1.WKPara" class="in_put_1" size="small">
+                        </el-input>
+                    </el-row>
+                </el-card>
+            </el-col>
+            <el-col :span="12">
+                <el-card class="monitorbox">
+                    <div slot="header" class="clearfix">
+                        <span >设备2参数</span>
+                    </div>
+                    <el-row style="margin-bottom: 10px">
+                        <span class="text2">IK模式 &nbsp;</span>
+                        <el-select v-model="testDevice_2.IKMode" class="in_put_1" size="small">
+                            <el-option
+                            v-for="item in IKMode"
+                            :key="item.value"
+                            :label="item.label"
+                            :value="item.value">
+                            </el-option>
+                        </el-select>
+                    </el-row>
+                    <el-row style="margin-bottom: 10px">
+                        <span class="text2">IK参数 &nbsp;</span>
+                        <el-input v-model="testDevice_2.IKPara" class="in_put_1" size="small">
+                        </el-input>
+                    </el-row>
+                   <el-row style="margin-bottom: 10px">
+                        <span class="text">WK模式 &nbsp;</span>
+                        <el-select v-model="testDevice_2.WKMode" class="in_put_1" size="small">
+                            <el-option
+                            v-for="item in WKMode"
+                            :key="item.value"
+                            :label="item.label"
+                            :value="item.value">
+                            </el-option>
+                        </el-select>
+                    </el-row>
+                    <el-row style="margin-bottom: 10px">
+                        <span class="text"> WK参数 &nbsp;</span>
+                        <el-input type="textarea" :rows=3 v-model="testDevice_2.WKPara" class="in_put_1" size="small">
+                        </el-input>
+                    </el-row>
+                </el-card>
+            </el-col>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="testDialogCancel()">取 消</el-button>
+                <el-button type="primary" @click="testDialogCancel()">确 定</el-button>
+            </span>
+        </el-dialog>
     </div>
 </template>
 
@@ -35,9 +215,66 @@ export default {
     },
     data(){
         return{
+            monitorDialogVisible:false,
+            testDialogVisible:false,
             monitorParaVisible:false,
             testParaVisible:false,
             uploadVisible:false,
+            testDevice_1:{
+                IKMode:'',
+                IKPara:'',
+                WKMode:'',
+                WKPara:'',
+            },
+            testDevice_2:{
+                IKMode:'',
+                IKPara:'',
+                WKMode:'',
+                WKPara:'',
+            },
+            IKMode:[
+                {value:'00-Work',label:'00-Work'},
+                {value:'01-Tdm',label:'01-Tdm'},
+                {value:'02-Aloha',label:'02-Aloha'},
+                {value:'03-Kde',label:'03-Kde'},
+            ],
+            WKMode:[
+                {value:'00-Work',label:'00-Work'},
+                {value:'01-Tdm',label:'01-Tdm'},
+                {value:'02-Aloha',label:'02-Aloha'},
+                {value:'03-Kde',label:'03-Kde'},
+            ],
+            monitorDevice_1:{
+                workMode:'',
+                groupType:'',
+                groupNumber:'',
+            },
+            monitorDevice_2:{
+                workMode:'',
+                groupType:'',
+                groupNumber:'',
+            },
+            workMode:[
+                {value:'10-XX2JZ',label:'10-XX2JZ'},
+                {value:'11-XX2JQ',label:'11-XX2JQ'},
+                {value:'12-XX2DD',label:'11-XX2DD'},
+                {value:'13-XX2YJ',label:'11-XX2YJ'},
+                {value:'14-XX2中心站ALOHA解TDM加',label:'14-XX2中心站ALOHA解TDM加'},
+                {value:'15-XX2中心站ALOHA',label:'15-XX2中心站ALOHA'},
+                {value:'16-XX2中心站TDM',label:'14-XX2中心站TDM'},
+            ],
+            workType:[
+                {value:'01-NET',label:'01-NET'},
+                {value:'06-PTOP',label:'06-PTOP'},
+                {value:'07-GROUPW',label:'07-GROUPW'},
+                {value:'08-YJ',label:'08-YJ'},
+            ],
+            workNumber:[
+                {value:'00-xx00',label:'00-xx00'},
+                {value:'01-xx01',label:'01-xx01'},
+                {value:'02-xx02',label:'02-xx02'},
+                {value:'03-xx03',label:'03-xx03'},
+            ],
             menu: {
                 option: [
                 '连接',
@@ -52,7 +289,6 @@ export default {
                 '查询设备状态',
                 '查询工作模式',
                 '设置工作模式',
-                '返回工作方式',
                 '通知本次呼叫结束',
                 '设置工作组号',
                 '查询工作组号',
@@ -89,6 +325,22 @@ export default {
         }
     },
     methods:{
+        monitorDialogCancel(){
+            console.log(this.monitorDevice_1)
+            this.monitorDialogVisible = false
+        },
+        monitorDialogSubmit(){
+            console.log(this.monitorDevice_2)
+            this.monitorDialogVisible = false
+        },
+        testDialogCancel(){
+            console.log(this.testDevice_1)
+            this.testDialogVisible = false
+        },
+        testDialogSubmit(){
+            console.log(this.testDevice_2)
+            this.testDialogVisible = false
+        },
         handleSelect (value) {
             switch(value){
                 case '1_0':
@@ -104,9 +356,39 @@ export default {
                 case '1_5':
                     break
                 case '2':
+                    this.monitorDialogVisible = true;
+                    break
+                case '5':
+                    this.testDialogVisible = true;
                     break
             }
         }
     }
 }
 </script>
+<style scoped>
+.monitorbox{
+    margin: 10px;
+}
+.in_put_1{
+    width: 180px;
+}
+.in_put_2{
+    width: 100px;
+    float:left;
+}
+.text{
+    margin-right: 5px;
+    padding-left: 5px;
+    float:left;
+}
+.text2{
+    margin-right: 10px;
+    padding-left: 5px;
+    padding-right: 4.5px;
+    float:left;
+}
+.monitorDialog{
+    height: 900px;
+}
+</style>
